3dca07451a93089c532eff6e886a401a00ef365e
Brad Lackey <bclackey@umd.edu>
2017-05-11 21:20:13 -0400

    Added setting mean to random number generation.

--- src/create_numbers.c
+++ src/create_numbers.c
@@ -9,27 +9,31 @@
 #include <time.h>
 #include <gmp.h>
 #include <mpfr.h>
+#include <math.h>
 #include "types.h"
 #include "npp-io.h"
 
 int main(int argc, char **argv){
   int i;
   unsigned int size, precision, seed;
+  double beta;
   gmp_randstate_t rand;
   array_t array;
   
   
-  if (argc < 3) {
-    fprintf(stderr,"Usage: %s <size of list> <precision> [<seed>]\n\n", argv[0]);
+  if (argc < 4) {
+    fprintf(stderr,"Usage: %s <size of list> <-log2(mean)> <precision> [<seed>]\n\n", argv[0]);
     return 2;
   }
   
   sscanf(argv[1],"%u", &(size));
   fprintf(stderr, "Size = %u\n", size);
-  sscanf(argv[2],"%u", &(precision));
+  sscanf(argv[2],"%lf", &(beta));
+  fprintf(stderr, "log2(mean) = -%.4f\n", beta);
+  sscanf(argv[3],"%u", &(precision));
   fprintf(stderr, "Precision = %u\n", precision);
-  if ( argc > 3 )
-    sscanf(argv[3],"%ul", &seed);
+  if ( argc > 4 )
+    sscanf(argv[4],"%ul", &seed);
   else
     seed = time(0);
   fprintf(stderr,"Seed = %u\n", seed);
@@ -38,19 +42,21 @@ int main(int argc, char **argv){
   
   gmp_randinit_mt(rand);
   gmp_randseed_ui(rand, seed);
+  beta = pow(2.0,-beta);
   for (i=0; i<size; ++i){
     mpfr_urandomb(array.number[i], rand);
     
     // The next line creates the uniform distribution with mean 1.
     //mpfr_mul_2ui(array.number[i], array.number[i], 1ul, MPFR_RNDN);
     
-    // The next two lines create the exponential distribution with mean 1.
-    mpfr_log(array.number[i], array.number[i], MPFR_RNDN);
-    mpfr_neg(array.number[i], array.number[i], MPFR_RNDN);
+    // The following lines create the exponential distribution with mean beta.
+    NUMBER_LOG(array.number[i], array.number[i]);
+    NUMBER_MUL_D(array.number[i], array.number[i], beta);
+    NUMBER_NEG(array.number[i], array.number[i]);
   }
   
   gmp_randclear(rand);
-//  qsort(array.number, size, sizeof(number_t), mpfr_cmp);
+  qsort(array.number, size, sizeof(number_t), mpfr_cmp);
   
   printArray(stdout, array);
   freeArray(array);
--- src/types.h
+++ src/types.h
@@ -18,15 +18,19 @@ typedef mpfr_t number_t;
 #define NUMBER_CLEAR mpfr_clear
 #define NUMBER_ZERO(n) mpfr_set_zero(n,1);
 #define NUMBER_SET(o,i) mpfr_set(o,i,MPFR_RNDN)
+#define NUMBER_SET_D(o,i) mpfr_set_d(o,i,MPFR_RNDN)
 #define NUMBER_PRINT(s,n) mpfr_out_str(s,10,0,n,MPFR_RNDN)
 #define NUMBER_SCAN(s,n) mpfr_inp_str(n,s,10,MPFR_RNDN)
 #define NUMBER_ADD(o,i1,i2) mpfr_add(o,i1,i2,MPFR_RNDN)
 #define NUMBER_SUB(o,i1,i2) mpfr_sub(o,i1,i2,MPFR_RNDN)
+#define NUMBER_MUL_D(o,i1,i2) mpfr_mul_d(o,i1,i2,MPFR_RNDN)
 #define NUMBER_HALF(o,i) mpfr_div_2ui(o,i,1ul,MPFR_RNDN)
 #define NUMBER_DUB(o,i) mpfr_mul_2ui(o,i,1ul,MPFR_RNDN)
 #define NUMBER_NEG(o,i) mpfr_neg(o,i,MPFR_RNDN)
 #define NUMBER_SGN mpfr_sgn
+#define NUMBER_LOG(o,i) mpfr_log(o,i,MPFR_RNDN)
 #define NUMBER_LOG2(o,i) mpfr_log2(o,i,MPFR_RNDN)
+#define NUMBER_EXP2(o,i) mpfr_exp2(o,i,MPFR_RNDN)
 
 typedef struct {
   unsigned int size;
